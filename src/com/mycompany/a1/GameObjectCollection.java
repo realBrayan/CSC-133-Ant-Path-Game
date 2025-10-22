976e2e2070851b733d18ad3914d60795f4f74b84
Brayan Mejia Nunez <132964348+realBrayan@users.noreply.github.com>
2025-10-21 21:10:13 -0700

    added ICollection, IIterable, and GameObjectCollection

---
+++ src/com/mycompany/a1/GameObjectCollection.java
@@ -0,0 +1,48 @@
+package com.mycompany.a1;
+
+import java.util.ArrayList;
+
+public class GameObjectCollection implements ICollection{
+
+	private ArrayList<GameObject> gameObjects;
+	
+	public GameObjectCollection() {
+		gameObjects = new ArrayList<>();
+	}
+	
+	@Override
+	public void add(GameObject newObject) {
+		gameObjects.add(newObject);
+	}
+	
+	public IIterator getIterator() {
+		return new GameObjectIterator();
+	}
+	
+	
+	private class GameObjectIterator implements IIterator{
+		private int currentIndex;
+
+		
+		public GameObjectIterator() {
+			currentIndex = 0;
+		}
+		
+		@Override
+		public boolean hasNext() {
+			// check if the current index is less than the size of the list
+			return currentIndex < gameObjects.size();
+		}
+
+		@Override
+		public GameObject getNext() {
+			
+			GameObject nextObject = gameObjects.get(currentIndex);
+			currentIndex++;
+			
+			return nextObject;
+		}
+	}
+	
+	
+}
