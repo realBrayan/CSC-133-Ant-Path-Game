6b7d462c975760be91d4327cae89ba0c26304d50
Brayan Mejia Nunez <132964348+realBrayan@users.noreply.github.com>
2025-11-01 21:38:05 -0700

    I forgot I had git setup for this project. Many changes have been made

---
+++ src/com/mycompany/a1/CollideWithFoodStationCommand.java
@@ -0,0 +1,23 @@
+package com.mycompany.a1;
+
+import com.codename1.ui.Command;
+import com.codename1.ui.events.ActionEvent;
+
+public class CollideWithFoodStationCommand extends Command {
+	
+	private GameWorld gw;
+
+    public CollideWithFoodStationCommand(GameWorld gw) {
+        super("Collide with Food Station");
+        this.gw = gw;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent evt) {
+        //gw.collideWithFoodStation();
+        gw.pretendFoodCollision();
+        
+        System.out.println("Collide with Food Station command invoked.");
+    }
+	
+}
